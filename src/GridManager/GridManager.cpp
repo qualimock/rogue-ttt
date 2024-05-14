2ddc4a5e377b9e7026e014e67041de3cde16a323
qualimock <qualimock@gmail.com>
2024-05-15 00:14:24 +0300

    Remove useless pointer processEvent from GridManager

--- src/GridManager/GridManager.cpp
+++ src/GridManager/GridManager.cpp
@@ -8,12 +8,6 @@
 
 namespace Grid
 {
-	void GridManager::processEvent(sf::Event &event,
-								   std::unique_ptr<IGrid>::pointer grid)
-	{
-		processEvent(event, &grid);
-	}
-
 	void GridManager::processEvent(sf::Event &event, Grid::BaseGrid &grid)
 	{
 		std::cout << grid.name() << std::endl;
