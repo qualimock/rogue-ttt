44df128ebdc3048c26f2d1f30e6281538a58587e
qualimock <qualimock@gmail.com>
2024-05-16 01:11:25 +0300

    Add Character class

---
+++ src/Entity/Character/Character.cpp
@@ -0,0 +1,33 @@
+#include "Character.hpp"
+
+#include <iostream>
+
+namespace Entity
+{
+	Character::Character(const sf::Vector2i &position,
+						 const sf::Vector2u &size,
+						 EType type)
+		: Entity(position, size)
+		, m_type(type)
+	{}
+
+	Character::Character(const Entity &entity, EType type)
+		: Entity(entity)
+		, m_type(type)
+	{}
+
+	Character::Character(Entity &&entity, EType type)
+		: Entity(entity)
+		, m_type(type)
+	{}
+
+	void Character::move(const sf::Vector2i &position)
+	{
+		setPosition(sf::Vector2f(position));
+	}
+
+	void Character::interact(const Entity &target)
+	{
+		std::cout << "INTERACTION" << std::endl;
+	}
+}
---
+++ src/Entity/Character/Character.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../Entity.hpp"
+
+namespace Entity
+{
+	class Character : public Entity
+	{
+	public:
+		enum class EType
+		{
+			Player,
+			NPC,
+			Enemy,
+
+			None
+		};
+
+		Character(const sf::Vector2i &position,
+				  const sf::Vector2u &size,
+				  EType type);
+		Character(const Entity &entity, EType type);
+		Character(Entity &&entity, EType type);
+
+		void move(const sf::Vector2i &position);
+
+		void interact(const Entity &target);
+
+	private:
+		EType m_type;
+	};
+}
